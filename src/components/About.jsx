03a3de810daae363799c345c60430151a6de1199
4nth0ny1 <acatullo4@gmail.com>
2021-08-27 00:18:52 -0500

    add styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,20 +3,6 @@ import React from "react";
 function About() {
   return (
     <div className="about">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,20 +4,7 @@ import React from "react";
 function Contact() {
   return (
     <div className="contact">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
 
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
