57d7abe05fe5dddf8920006c40feac6bac39a509
4nth0ny1 <acatullo4@gmail.com>
2021-09-15 15:45:11 -0500

    add minor variations no change

--- src/components/Comments/CommentContainer.jsx
+++ src/components/Comments/CommentContainer.jsx
@@ -1,9 +1,10 @@
 import Comment from './Comment';
 
 const CommentContainer = (props) => {
-
+    
     const commentArray = props.comments.map(comment => {
-        return <Comment key={comment.id} comment={props.comment} addComment={props.addComment} deleteComment={props.deleteComment}/>
+        return <Comment key={comment.id} comment={comment} />
+        
     })
 
     return(
